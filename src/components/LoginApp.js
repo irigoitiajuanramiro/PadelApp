cc62a71dd280c682e31bee3076b4009f2e3df09c
Juan Ramiro <irigoitiajuanramiro@hotmail.com>
2020-09-14 22:40:01 -0300

    Login y login celular

--- src/components/LoginApp.js
+++ src/components/LoginApp.js
@@ -1,43 +1,36 @@
 import React from "react";
 
 import {
-  Container,
   FormControl,
   InputLabel,
   Input,
   FormHelperText,
-  Button,
 } from "@material-ui/core";
 import "../index.css";
 const LoginApp = () => {
   return (
     <div className="container">
+      <div className="subcontainer">
+        <div className="sign">
+          <h2>Sign In</h2>
+        </div>
 
-      <div className="sign">
-        <h2>Sign In</h2>
+        <FormControl>
+          <InputLabel htmlFor="email">Email address</InputLabel>
+          <Input id="email" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">Email personal.</FormHelperText>
+        </FormControl>
+
+        <FormControl>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input id="password" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            Entre 6 y 12 caracteres.
+          </FormHelperText>
+        </FormControl>
+        <button className="button">Submit</button>
       </div>
-
-      <FormControl fullWidth>
-        <InputLabel htmlFor="email">Email address</InputLabel>
-        <Input id="email" aria-describedby="my-helper-text" />
-        <FormHelperText id="my-helper-text">Email personal.</FormHelperText>
-      </FormControl>
-
-
-      <FormControl fullWidth>
-        <InputLabel htmlFor="password">Password</InputLabel>
-        <Input id="password" aria-describedby="my-helper-text" />
-        <FormHelperText id="my-helper-text">
-          Entre 6 y 12 caracteres.
-            </FormHelperText>
-      </FormControl>
-
-
-      <Button color="secondary" variant="contained">
-        Submit
-          </Button>
     </div>
-
   );
 };
 
