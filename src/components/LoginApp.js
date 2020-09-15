3697411337dcbf5c5978fbeda8459b5346a8f312
Nahuel <nahueljaume@gmail.com>
2020-09-14 21:09:36 -0300

    flex container

--- src/components/LoginApp.js
+++ src/components/LoginApp.js
@@ -12,33 +12,32 @@ import "../index.css";
 const LoginApp = () => {
   return (
     <div className="container">
-      <Container>
-        <div className="sign">
-          <h2>Sign In</h2>
-        </div>
-        <div className="formulario">
-          <FormControl>
-            <InputLabel htmlFor="email">Email address</InputLabel>
-            <Input id="email" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">Email personal.</FormHelperText>
-          </FormControl>
-        </div>
-        <div className="formulario">
-          <FormControl>
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input id="password" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">
-              Entre 6 y 12 caracteres.
+
+      <div className="sign">
+        <h2>Sign In</h2>
+      </div>
+
+      <FormControl fullWidth>
+        <InputLabel htmlFor="email">Email address</InputLabel>
+        <Input id="email" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">Email personal.</FormHelperText>
+      </FormControl>
+
+
+      <FormControl fullWidth>
+        <InputLabel htmlFor="password">Password</InputLabel>
+        <Input id="password" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">
+          Entre 6 y 12 caracteres.
             </FormHelperText>
-          </FormControl>
-        </div>
-        <div className="button">
-          <Button color="secondary" variant="contained">
-            Submit
+      </FormControl>
+
+
+      <Button color="secondary" variant="contained">
+        Submit
           </Button>
-        </div>
-      </Container>
     </div>
+
   );
 };
 
